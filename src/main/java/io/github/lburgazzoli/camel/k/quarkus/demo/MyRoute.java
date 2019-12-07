135239d8205dccd7a880f760f9135ded888bd77b
lburgazzoli <lburgazzoli@gmail.com>
2019-12-07 17:28:14 +0100

    sample route

---
+++ src/main/java/io/github/lburgazzoli/camel/k/quarkus/demo/MyRoute.java
@@ -0,0 +1,22 @@
+package io.github.lburgazzoli.camel.k.quarkus.demo;
+
+import org.apache.camel.BindToRegistry;
+import org.apache.camel.Processor;
+import org.apache.camel.PropertyInject;
+import org.apache.camel.builder.RouteBuilder;
+
+public class MyRoute extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("timer:tick")
+            .process("myProcessor")
+            .to("log:demo");
+    }
+
+    @BindToRegistry
+    public static Processor myProcessor(
+            @PropertyInject("my.processor.message") String message) {
+
+        return e -> e.getMessage().setBody(message);
+    }
+}
